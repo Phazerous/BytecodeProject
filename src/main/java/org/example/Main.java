af83de2ff3bc768277f5b1e20c4b76e70fe0fe79
Phazerous <nphazerous@gmail.com>
2022-11-04 21:52:12 +0600

    MainClassCode Added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,19 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+
+public class Main {
+    static public void main(String args[]) throws Exception {
+        EAN13Generator generator = new EAN13Generator();
+        // Additional arguments
+        generator.setImageHeight(120);
+        generator.setImageWidth(130);
+        generator.setStartYPos(0);
+        generator.setStartXPos(0);
+        generator.setDefaultGuardHeight(120);
+        generator.setDefaultLineHeight(80);
+
+        ImageIO.write(generator.generateEAN13("9443044102032"), "PNG", new File("barcodeTest.PNG"));
     }
 }
